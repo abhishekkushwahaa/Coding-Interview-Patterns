4adbcf7c6d49bc4d533238ba7a04d9e90ea5142d
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-01-15 08:13:55 +0530

    Container-with-most-water

--- Two-Pointers/MostWater.js
+++ Two-Pointers/MostWater.js
@@ -1,2 +1,26 @@
 // LeetCode: https://leetcode.com/problems/container-with-most-water/
 // Example: maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]) => 49
+
+var maxArea = function(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxWater = 0;
+
+  while (left < right) {
+    let width = right - left;
+    let h = Math.min(height[left], height[right]);
+
+    let water = width * h;
+    maxWater = Math.max(maxWater, water);
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxWater;
+};
+
+// Time Complexity = O(n)
