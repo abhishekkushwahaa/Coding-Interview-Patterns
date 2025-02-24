a131c8ddc67c862969c8b0b14ed47a47c1d33596
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-02-24 10:17:25 +0530

    sum btw range

--- Prefix-Sums/sum-btw-range.go
+++ Prefix-Sums/sum-btw-range.go
@@ -1,1 +1,28 @@
+package main
 
+import "fmt"
+
+func PrefixSum(arr []int) []int {
+	n := len(arr)
+	prefix := make([]int, n)
+	prefix[0] = arr[0]
+	for i := 1; i < n; i++ {
+		prefix[i] = prefix[i-1] + arr[i]
+	}
+	return prefix
+}
+
+func RangeSum(prefix []int, L, R int) int {
+	if L == 0 {
+		return prefix[R]
+	}
+	return prefix[R] - prefix[L-1]
+}
+
+func main() {
+	arr := []int{2, 4, 6, 8, 10}
+	prefix := PrefixSum(arr)
+	L, R := 1, 3
+	result := RangeSum(prefix, L, R)
+	fmt.Printf("Sum between index %d and %d: %d\n", L, R, result)
+}
