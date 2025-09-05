63abbc590c3694a7a846cf0344ce1685b95a8a3e
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-09-05 22:35:32 +0530

    midpoint linkedlist

---
+++ Linked-Lists/ll-midpoint.js
@@ -0,0 +1,15 @@
+function midPoint(head) {
+  if (!head) {
+    return null
+  }
+
+  let slow = head
+  let fast = head
+
+  while (fast != null && fast.next != null) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  return slow
+}
