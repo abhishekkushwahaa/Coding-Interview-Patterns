3381fb4113e7b033d65b8af4011ff7a86536d934
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-11-14 09:02:05 +0530

    midpoint

--- Practice/linkedlist.js
+++ Practice/linkedlist.js
@@ -5,46 +5,37 @@ class Node {
   }
 }
 
-function addTwoNumbers(l1, l2) {
-  let dummy = new Node(0)
-  let current = dummy
-  let carry = 0
+function midPoint(head) {
 
-  while (l1 !== null || l2 !== null || carry > 0) {
-    let x = (l1 !== null) ? l1.value : 0
-    let y = (l2 !== null) ? l2.value : 0
+  if (!head) return null;
 
-    let sum = x + y + carry;
+  let slow = head;
+  let fast = head;
 
-    carry = Math.floor(sum / 10)
-
-    current.next = new Node(sum % 10)
-    current = current.next
-
-    if (l1 !== null) l1 = l1.next
-    if (l2 !== null) l2 = l2.next
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next
+    fast = fast.next.next
   }
-  return dummy.next
+
+  return slow;
 }
 
-function PrintList(head) {
+function printList(head) {
   let current = head
-  let value = []
+  let result = []
+
   while (current !== null) {
-    value.push(current.value)
+    result.push(current.value)
     current = current.next
   }
-  console.log(value.join(" -> "))
+  console.log(result.join(" -> "))
 }
 
-// Example
-let l1 = new Node(2);
-l1.next = new Node(4);
-l1.next.next = new Node(3);
+let list = new Node(2);
+list.next = new Node(3);
+list.next.next = new Node(5);
+list.next.next.next = new Node(7);
 
-let l2 = new Node(5);
-l2.next = new Node(6);
-l2.next.next = new Node(4);
-
-let result = addTwoNumbers(l1, l2);
-PrintList(result);
+let midPointofList = midPoint(list)
+// printList(midPointofList.result);
+console.log(midPointofList.value)
