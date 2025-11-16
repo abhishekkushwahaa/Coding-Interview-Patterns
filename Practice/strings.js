52313df8ecc55258cabb1145fd69d368d5d8de3d
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2025-11-16 08:41:06 +0530

    reverseStr

--- Practice/strings.js
+++ Practice/strings.js
@@ -1,1 +1,16 @@
-console.log("Hello from String!")
+function reverseString(str) {
+  let arr = str.split("")
+  let left = 0; let right = arr.length - 1;
+
+  while (left < right) {
+    // Swap Char
+    [arr[left], arr[right]] = [arr[right], arr[left]]
+    left++
+    right--
+  }
+
+  return arr.join("")
+}
+
+let reverseStr = reverseString("balji")
+console.log(reverseStr);
